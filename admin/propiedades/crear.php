43a0a46b744921855036e54f45bcd8fe33851098
CintiaUtrera <cintiaa_utrera@outlook.es>
2023-01-25 18:34:20 -0300

    Creacion de formulario para nueva propiedad

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,11 +1,56 @@
 <?php 
 
     require '../../includes/funciones.php';
-    incluirTemplate('header', $inicio = true);
+    incluirTemplate('header');
 ?>
 
     <main class="contenedor seccion">
-        <h1>Titulo Página</h1>
+        <h1>Crear</h1>
+
+        <a href="/admin" class="boton boton-verde">volver</a>
+
+        <form class="formulario">
+            <fieldset>
+                <legend>Información General</legend>
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" placeholder="Precio">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+            
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion"></textarea>
+
+            </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+            
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" placeholder="Ej: 2" min="1" max="6">
+
+                <label for="estacionamiento">Estacionamiento</label>
+                <input type="number" id="estacionamiento" placeholder="Ej: 1" min="1" max="5">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+            <select>
+                <option value="1">Cintia Utrera</option>
+                <option value="2">Emanuel Campos</option>
+                
+            </select>
+
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="boton boton-verde"
+        </form>
     </main>
 
     <?php 
