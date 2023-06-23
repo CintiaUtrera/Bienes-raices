7507d9da496da62641c8500bb4f05dc5a425a912
Cintia <cintiaa_utrera@outlook.es>
2023-06-23 12:41:47 -0300

    creando tabla para mostrar propiedades desde la db

--- admin/index.php
+++ admin/index.php
@@ -13,6 +13,30 @@
         <?php endif; ?>
 
         <a href="../admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td> <img src="/imagenes/10be6a678417dacbd3132f26fa575767.jpg" class="imagen-tabla"> </td>
+                    <td>$1500000</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     
     </main>
 
