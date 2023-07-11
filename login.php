e46f770afe35235518d7f74defc17a271f7e802d
Cintia <cintiaa_utrera@outlook.es>
2023-07-11 12:53:17 -0300

    validaciones al formulario

--- login.php
+++ login.php
@@ -1,4 +1,29 @@
 <?php 
+
+    //conectar db
+    require 'includes/config/database.php';
+    $db = conectarDB();
+
+    // Autenticar el Usuario
+    $errores = [];
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+
+        $email = mysqli_real_escape_string($db, filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL));
+
+        $password = mysqli_real_escape_string($db, $_POST['password']);
+
+        if(!$email){
+            $errores[] = "El Email es obligatorio o no es válido";
+        }
+
+        if(!$password){
+            $errores[] = "El Password es obligatorio";
+        }
+    }
+
+
 require 'includes/funciones.php';
 incluirTemplate('header');
 ?>
@@ -6,15 +31,21 @@ incluirTemplate('header');
 <main class="contenedor seccion contenido-centrado">
     <h1>Iniciar Sesión</h1>
 
-    <form class="formulario">
+    <?php foreach($errores as $error): ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php endforeach; ?>
+
+    <form method="POST" class="formulario">
         <fieldset>
             <legend>Email y Password</legend>
             
             <label for="email">E-mail</label>
-            <input type="email" placeholder="Tu Email" id="email">
+            <input type="email" name="email" placeholder="Tu Email" id="email">
 
             <label for="password">Password</label>
-            <input type="password" placeholder="Tu Password" id="password">
+            <input type="password" name="password" placeholder="Tu Password" id="password">
 
         </fieldset>
 
