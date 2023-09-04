1afe09e966690cac50315353516281127e6376ec
Cintia <cintiaa_utrera@outlook.es>
2023-09-04 12:53:46 -0300

    obtener y listar propiedades

--- anuncio.php
+++ anuncio.php
@@ -1,4 +1,6 @@
 <?php 
+    require 'includes/app.php';
+    use App\Propiedad;
 
     $id = $_GET['id'];
     $id = filter_var($id, FILTER_VALIDATE_INT);
@@ -7,54 +9,36 @@
         header('Location: /bienesraices/index.php');
     }
 
-        // Incluir la conexion 
-        require 'includes/app.php';
-        $db = conectarDB();
-
-        //Consultar 
-        $query = "SELECT * FROM propiedades WHERE id = {$id}";
-
-        // obtener los resultados
-        $resultado = mysqli_query($db, $query);
-
-        if($resultado->num_rows === 0) {
-            header('Location: /bienesraices/index.php');
-        }
-        $propiedad = mysqli_fetch_assoc($resultado);
-
-
-
+    $propiedad = Propiedad::find($id);
     
     incluirTemplate('header');
 ?>
 
     <main class="contenedor seccion contenido-centrado">
-        <h1><?php echo $propiedad['titulo']; ?></h1>
+        <h1><?php echo $propiedad->titulo; ?></h1>
         
-        <img class="imagen-anuncio" loading="lazy" src="/bienesraices/imagenes/<?php echo $propiedad['imagen']; ?>" alt="imagen de la propiedad">
+        <img class="imagen-anuncio" loading="lazy" src="/bienesraices/imagenes/<?php echo $propiedad->imagen; ?>" alt="imagen de la propiedad">
 
         <div class="resumen-propiedad">
-            <p class="precio"><?php echo "$" . number_format($propiedad['precio']); ?></p>
+            <p class="precio">$<?php echo $propiedad->precio; ?></p>
             <ul class="iconos-caracteristicas">
                 <li>
                     <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                    <p><?php echo $propiedad['wc']; ?></p>
+                    <p><?php echo $propiedad->wc; ?></p>
                 </li>
                 <li>
                     <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                    <p><?php echo $propiedad['estacionamiento']; ?></p>
+                    <p><?php echo $propiedad->estacionamiento; ?></p>
                 </li>
                 <li>
                     <img class="icono"  loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
-                    <p><?php echo $propiedad['habitaciones']; ?></p>
+                    <p><?php echo $propiedad->habitaciones; ?></p>
                 </li>
             </ul>
-            <?php echo $propiedad['descripcion']; ?>
+            <?php echo $propiedad->descripcion; ?>
         </div>
     </main>
 
     <?php 
-    mysqli_close($db);
-    
     incluirTemplate('footer');
     ?>
--- includes/templates/anuncios.php
+++ includes/templates/anuncios.php
@@ -1,52 +1,41 @@
 <?php
-
-    // Incluir la conexion 
-    $db = conectarDB();
-
-    //Consultar 
-    $query = "SELECT * FROM propiedades LIMIT {$limite}";
-
-    // obtener los resultados
-    $resultado = mysqli_query($db, $query);
+    use App\Propiedad;
+    $propiedades = Propiedad::all();
 ?>
 
 
 
 <div class="contenedor-anuncios">
-    <?php while($propiedad = mysqli_fetch_assoc($resultado)): ?>
+    <?php foreach($propiedades as $propiedad) { ?>
             <div class="anuncio">
                 <picture>
-                    <img loading="lazy" src="/bienesraices/imagenes/<?php echo $propiedad['imagen']; ?>" alt="anuncio">
+                    <img loading="lazy" src="/bienesraices/imagenes/<?php echo $propiedad->imagen; ?>" alt="anuncio">
 
                 <div class="contenido-anuncio">
-                    <h3><?php echo $propiedad['titulo']; ?></h3>
-                    <p><?php echo $propiedad['descripcion']; ?></p>
-                    <p class="precio"><?php echo "$" . number_format($propiedad['precio']); ?></p>
+                    <h3><?php echo $propiedad->titulo; ?></h3>
+                    <p><?php echo $propiedad->descripcion; ?></p>
+                    <p class="precio">$<?php echo $propiedad->precio; ?></p>
 
                     <ul class="iconos-caracteristicas">
                         <li>
                             <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                            <p><?php echo $propiedad['wc']; ?></p>
+                            <p><?php echo $propiedad->wc; ?></p>
                         </li>
                         <li>
                             <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                            <p><?php echo $propiedad['estacionamiento']; ?></p>
+                            <p><?php echo $propiedad->estacionamiento; ?></p>
                         </li>
                         <li>
                             <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
-                            <p><?php echo $propiedad['habitaciones']; ?></p>
+                            <p><?php echo $propiedad->habitaciones; ?></p>
                         </li>
                     </ul>
 
-                    <a href="/bienesraices/anuncio.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">
+                    <a href="/bienesraices/anuncio.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">
                         Ver Propiedad
                     </a>
                 </div><!--.contenido-anuncio-->
             </div><!--anuncio-->
-        <?php endwhile; ?>
+        <?php } ?>
         </div> <!--.contenedor-anuncios-->
 
-<?php
-// Cerrar la conexion
-    mysqli_close($db);
-?>
